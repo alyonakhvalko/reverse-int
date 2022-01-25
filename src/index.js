40e3a6320e3fd2d51b9d10fb502b895c208d0d1e
Alyona Khvalko <85245607+alyonakhvalko@users.noreply.github.com>
2022-01-25 20:42:04 +0300

    feat: upload solution

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
  module.exports = function reverse (n) {
-    let result = 0;
-    while (n) {
-      result = result * 10 + n % 10;
-    n = Math.floor(n / 10);
-    }
-    return result;
+   return (parseFloat (n.toString().split('').reverse().join('')));
 } 
 
